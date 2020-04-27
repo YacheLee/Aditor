db74e7f48455fe6c1dfe40545a3615d5d31d6be2
ya-che.li <ya-che.li@and.digital>
2020-04-27 07:34:18 +0100

    Updates

---
+++ precache-manifest.084eac1f90eca1e663ee6a83fdc46ee0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fffa5f1d394fabfa3d163717151ab048",
+    "url": "/YacheLee/Aditor/index.html"
+  },
+  {
+    "revision": "b703d1f85596f10780f7",
+    "url": "/YacheLee/Aditor/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "0d6a23c2b06d8ec1552c",
+    "url": "/YacheLee/Aditor/static/js/2.3c41252a.chunk.js"
+  },
+  {
+    "revision": "68ba00d5eb083746d913686923a3d904",
+    "url": "/YacheLee/Aditor/static/js/2.3c41252a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b703d1f85596f10780f7",
+    "url": "/YacheLee/Aditor/static/js/main.c288c7d0.chunk.js"
+  },
+  {
+    "revision": "a3bf5ed3a7cefd568ea2",
+    "url": "/YacheLee/Aditor/static/js/runtime-main.6d9410c3.js"
+  }
+]);
