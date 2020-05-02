7fbcdea76924ce06a6377db61a1bf2a735a62435
ya-che.li <ya-che.li@and.digital>
2020-05-02 23:44:16 +0100

    remove config.js

--- src/config.js
+++
@@ -1,5 +0,0 @@
-export const BLACK_COLOR = '#42526E';
-export const GREY_COLOR = '#ECEDF0';
-export const BORDER_RADIUS = 5;
-export const DEFAULT_COLOR = '#22194D';
-export const DEFAULT_ICON_FONT_SIZE = '24px';
