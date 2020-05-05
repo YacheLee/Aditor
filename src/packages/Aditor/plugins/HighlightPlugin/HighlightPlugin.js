1ec9cc4ca6d0cef017cc5ae72541d22456c2b174
ya-che.li <ya-che.li@and.digital>
2020-05-05 14:17:45 +0100

    Use Decoration to make the plugin better!

--- src/packages/Aditor/plugins/HighlightPlugin/HighlightPlugin.js
+++ src/packages/Aditor/plugins/HighlightPlugin/HighlightPlugin.js
@@ -1,124 +1,44 @@
-import { AllSelection, Plugin } from 'prosemirror-state';
-import modules from './HighlightPlugin.module.css';
+import { Plugin } from 'prosemirror-state';
+import { Decoration, DecorationSet } from 'prosemirror-view';
+import styles from './HighlightPlugin.module.css';
 
-function getStart({ isFirst, fromTextOffset, isAllSelection }) {
-  if (isAllSelection) {
-    return 0;
-  } else if (isFirst) {
-    return fromTextOffset;
-  }
-  return 0;
-}
-
-function getEnd({ isLast, textLength, toTextOffset, isAllSelection }) {
-  if (isAllSelection) {
-    return textLength;
-  } else if (isLast) {
-    return toTextOffset;
-  }
-  return textLength;
-}
-
-function getTextNodes(dom) {
-  if (dom.childNodes.length === 0) {
-    return [];
-  } else {
-    let arr = [];
-    [...dom.childNodes].forEach((child) => {
-      if (child.nodeType === window.Node.TEXT_NODE) {
-        arr.push(child);
-      }
-      arr = arr.concat(getTextNodes(child));
-    });
-    return arr;
-  }
-}
-
-function addOverlay(editorView) {
-  const { $from, $to } = editorView.state.selection;
-  const isAllSelection = editorView.state.selection instanceof AllSelection;
-  const fromTextOffset = $from.parentOffset;
-  const toTextOffset = $to.parentOffset;
-
-  const nodes = [];
-  editorView.state.doc.nodesBetween($from.pos, $to.pos, function (
-    node,
-    nodePos
-  ) {
-    if (node.isBlock) {
-      nodes.push({ node, nodePos });
-    }
-  });
-
-  nodes.forEach(function ({ node, nodePos }, index) {
-    const isFirst = index === 0;
-    const isLast = index === nodes.length - 1;
-    const dom = editorView.nodeDOM(nodePos);
-
-    const textNodes = getTextNodes(dom);
-    const firstTextNode = textNodes[0];
-
-    const textLength = firstTextNode.length;
-    const textRange = document.createRange();
-    const _start = getStart({ isFirst, fromTextOffset, isAllSelection });
-    const _end = getEnd({ isLast, textLength, toTextOffset, isAllSelection });
-    textRange.setStart(firstTextNode, _start);
-    textRange.setEnd(firstTextNode, _end);
-    const { top, left, width, height } = textRange.getBoundingClientRect();
-
-    const overlay = document.createElement('div');
-    overlay.className = 'overlay';
-    overlay.style.cursor = 'text';
-    overlay.style.position = 'absolute';
-    overlay.style.display = '';
-    overlay.style.left = left + 'px';
-    overlay.style.top = top + 'px';
-    overlay.style.width = `${width}px`;
-    overlay.style.height = `${height}px`;
-    overlay.style.backgroundColor = 'blue';
-    overlay.style.opacity = '0.15';
-    overlay.addEventListener('click', () => {
-      overlay.style.display = 'none';
-    });
-    document.body.appendChild(overlay);
-  });
-}
-
-function removeOverlay() {
-  const elements = document.querySelectorAll('.overlay');
-  elements.forEach((e) => {
-    e.remove();
-  });
-}
-
-function sync(editorView) {
-  removeOverlay(editorView);
-  addOverlay(editorView);
+function getState(doc, from, to) {
+  const decorations = [Decoration.inline(from, to, { class: styles.highlight })]
+  return DecorationSet.create(doc, decorations);
 }
 
 class View {
   constructor(editorView) {
     this.editorView = editorView;
-    editorView.dom.classList.add(modules.highlightPlugin);
-  }
-
-  update(editorView) {
-    const { from, to } = editorView.state.selection;
-    if (from !== to) {
-      sync(editorView);
-    } else {
-      removeOverlay();
-    }
+    editorView.dom.classList.add(styles.editorView);
   }
 }
 
-function HighlightPlugin() {
-  const plugin = new Plugin({
+function MyPlugin() {
+  return new Plugin({
     view(editorView) {
       return new View(editorView);
+    },
+    state: {
+      init(_, {doc}) {
+        return getState(doc);
+      },
+      apply(tr) {
+        const {selection, doc} = tr;
+        const { from, to } = selection;
+        if (from !== to) {
+          return getState(doc, from, to);
+        } else {
+          return getState(doc, 0, 0);
+        }
+      }
+    },
+    props: {
+      decorations(state) {
+        return this.getState(state);
+      }
     }
   });
-
-  return plugin;
 }
-export default HighlightPlugin;
+
+export default MyPlugin;
