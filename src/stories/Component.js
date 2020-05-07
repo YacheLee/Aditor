fb339342ff1d0f0a2532819c7da19e819ea3d9fd
ya-che.li <ya-che.li@and.digital>
2020-05-07 12:54:23 +0100

    separate the files

---
+++ src/stories/Component.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Aditor from '../packages/Aditor';
+
+export default function Component({defaultValue}){
+  const [value, setValue] = React.useState(defaultValue);
+  return <div className="App">
+    <Aditor id="aditor" defaultValue={value} onChange={value=>{
+      setValue(value);
+    }} />
+    <div test_id='aditor-state'>
+      {JSON.stringify(value)}
+    </div>
+  </div>
+}
