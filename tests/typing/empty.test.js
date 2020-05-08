e9e89f3a37b88662c5d54879ce7c799b4ca7f2fa
ya-che.li <ya-che.li@and.digital>
2020-05-08 11:50:17 +0100

    refined

--- tests/typing/empty.test.js
+++ tests/typing/empty.test.js
@@ -1,14 +1,13 @@
 import { Selector } from 'testcafe';
+import { SELECTOR_EDITOR } from '../config';
+import Utils from '../utils';
 
 fixture `Getting Started`
   .page `http://localhost:9009/iframe.html?id=aditor--empty`;
 
 test('As a user, I wanna type English so that I am able to see the English I typed.', async t => {
-  const EDITOR = `.ProseMirror`;
   const text = 'Hello world';
 
-  await t.typeText(EDITOR, text);
-
-  const data = await Selector('[test_id=aditor-state]').innerText;
-  await t.expect(JSON.parse(data)).eql([{"type":"paragraph","content":[{"type":"text","text": text}]}]);
+  await t.typeText(SELECTOR_EDITOR, text);
+  await t.expect(await Utils.getData()).eql([{"type":"paragraph","content":[{"type":"text","text": text}]}]);
 });
