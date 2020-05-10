0a546c47dc0b3d76d7e7f037dab1890ed45adaa4
ya-che.li <ya-che.li@and.digital>
2020-05-10 22:39:22 +0100

    use re-resizable

--- src/packages/Aditor/plugins/ImagePlugin/components/Image.js
+++ src/packages/Aditor/plugins/ImagePlugin/components/Image.js
@@ -1,18 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Resizable } from 're-resizable';
+
+const CustomHandle = props => (
+  <div
+    style={{
+      background: '#fff',
+      borderRadius: '2px',
+      border: '1px solid #ddd',
+      height: '100%',
+      width: '100%',
+      padding: 0,
+    }}
+    {...props}
+  />
+);
 
 function Image({node, editorView, getPos}){
-  const {src, alt, title, width, height} = node.attrs;
-  return <span onMouseUp={()=>{
-    console.log("onMouseUp");
-  }}>
+  const {src, alt, title, width: _width, height: _height} = node.attrs;
+  const [width, setWidth] = useState(_width);
+  const [height, setHeight] = useState(_height);
+
+  return  <Resizable
+    size={{ width, height }}
+    style={{display: "inline-block"}}
+    handleComponent={{
+      bottomRight: <CustomHandle />,
+      topRight: <CustomHandle />,
+      topLeft: <CustomHandle />,
+      bottomLeft: <CustomHandle />,
+      bottom: <CustomHandle />,
+      right: <CustomHandle />,
+      top: <CustomHandle />,
+      left: <CustomHandle />,
+    }}
+    onResizeStop={(event, direction, refToElement, delta)=>{
+      setWidth(width + delta.width);
+      setHeight(height + delta.height);
+    }}
+  >
     <img
+      width={width}
+      height={height}
       src={src}
       alt={alt}
       title={title}
-      width={width}
-      height={height}
     />
-  </span>
+  </Resizable>
 }
 
 export default Image;
