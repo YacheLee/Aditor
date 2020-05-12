8f33e5944fb2d6016363b5bc4460ade07db16f31
ya-che.li <ya-che.li@and.digital>
2020-05-12 20:20:07 +0100

    refined

--- src/packages/Aditor/plugins/MediaSinglePlugin/MediaSinglePlugin.js
+++ src/packages/Aditor/plugins/MediaSinglePlugin/MediaSinglePlugin.js
@@ -1,16 +1,10 @@
 import { Plugin, PluginKey } from 'prosemirror-state';
-import { updateLayout } from './commands';
 
 const pluginKey = new PluginKey('MediaSinglePlugin');
 
 function MediaSinglePlugin(){
   return new Plugin({
     key: pluginKey,
-    props: {
-      handleClickOn: (editorView, pos, node, nodePos)=>{
-        // updateLayout({pos: nodePos, editorView, node, layout: "left"});
-      }
-    }
   });
 }
 
