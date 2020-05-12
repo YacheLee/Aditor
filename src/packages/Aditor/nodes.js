cc2115df66d9410642882076c434a3a30cf92853
ya-che.li <ya-che.li@and.digital>
2020-05-12 20:33:06 +0100

    refine heading nodes

--- src/packages/Aditor/nodes.js
+++ src/packages/Aditor/nodes.js
@@ -1,5 +1,6 @@
 import {name as mediaSingleNodeName, node as mediaSingleNode} from './plugins/MediaSinglePlugin/node';
 import {name as imageNodeName, node as imageNode} from './plugins/ImagePlugin/node';
+import {name as headingNodeName, node as headingNode} from './plugins/HeadingPlugin/node';
 
 export default {
   doc: {
@@ -15,23 +16,7 @@ export default {
   },
   [mediaSingleNodeName]: mediaSingleNode,
   [imageNodeName]: imageNode,
-  heading: {
-    attrs: { level: { default: 1 } },
-    content: 'inline*',
-    group: 'block',
-    defining: true,
-    parseDOM: [
-      { tag: 'h1', attrs: { level: 1 } },
-      { tag: 'h2', attrs: { level: 2 } },
-      { tag: 'h3', attrs: { level: 3 } },
-      { tag: 'h4', attrs: { level: 4 } },
-      { tag: 'h5', attrs: { level: 5 } },
-      { tag: 'h6', attrs: { level: 6 } }
-    ],
-    toDOM(node) {
-      return ['h' + node.attrs.level, 0];
-    }
-  },
+  [headingNodeName]: headingNode,
   text: {
     group: 'inline',
     inline: true
