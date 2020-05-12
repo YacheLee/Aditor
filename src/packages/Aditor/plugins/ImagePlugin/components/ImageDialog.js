aa55de6002af090adcf39f92b21739cc2f489fd4
ya-che.li <ya-che.li@and.digital>
2020-05-12 20:00:43 +0100

    extract the getLayout function

--- src/packages/Aditor/plugins/ImagePlugin/components/ImageDialog.js
+++ src/packages/Aditor/plugins/ImagePlugin/components/ImageDialog.js
@@ -8,7 +8,7 @@ import DialogTitle from '@material-ui/core/DialogTitle';
 import Paper from '@material-ui/core/Paper';
 import Draggable from 'react-draggable';
 import { setImageAttrs } from '../commands';
-import { updateLayout } from '../../MediaSinglePlugin/commands';
+import { getLayout, updateLayout } from '../../MediaSinglePlugin/commands';
 
 function PaperComponent(props) {
   return (
@@ -16,12 +16,6 @@ function PaperComponent(props) {
       <Paper {...props} />
     </Draggable>
   );
-}
-
-function getLayout(mediaSingleNode){
-  if(!mediaSingleNode) return 'left';
-
-  return mediaSingleNode.node.attrs.layout;
 }
 
 function Component({node, editorView, getPos, onClose}){
--- src/packages/Aditor/plugins/MediaSinglePlugin/commands.js
+++ src/packages/Aditor/plugins/MediaSinglePlugin/commands.js
@@ -3,3 +3,9 @@ import { setAttrs } from '../../commands';
 export function updateLayout({pos, editorView, node, layout}){
   setAttrs({pos, editorView, node, attrs: {layout}});
 }
+
+export function getLayout(mediaSingleNode){
+  if(!mediaSingleNode) return 'left';
+
+  return mediaSingleNode.node.attrs.layout;
+}
