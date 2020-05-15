77615fc94d81255996f09f26cb266d981733bf3d
ya-che.li <ya-che.li@and.digital>
2020-05-15 14:14:28 +0100

    selectNode

--- src/packages/Aditor/plugins/ImagePlugin/commands.js
+++ src/packages/Aditor/plugins/ImagePlugin/commands.js
@@ -1,6 +1,15 @@
+import { NodeSelection } from 'prosemirror-state';
+
 export function setImageAttrs({pos, editorView, node, attrs}){
   const tr = editorView.state.tr;
   const toArr = {...node.attrs, ...attrs};
   tr.setNodeMarkup(pos, null, toArr);
+  editorView.dispatch(tr);
+}
+
+export function selectNode(editorView, pos){
+  const nodeSelection = NodeSelection.create(editorView.state.doc, pos);
+  const tr = editorView.state.tr;
+  tr.setSelection(nodeSelection);
   editorView.dispatch(tr);
 }
