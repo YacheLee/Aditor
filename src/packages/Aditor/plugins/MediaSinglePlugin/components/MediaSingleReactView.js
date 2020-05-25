0d5e6bfa05f11538480ec6b0e5e1630af8a3fe82
scott <mymislife@gmail.com>
2020-05-25 09:56:18 +0100

    fix bug for focusing the image

--- src/packages/Aditor/plugins/MediaSinglePlugin/components/MediaSingleReactView.js
+++ src/packages/Aditor/plugins/MediaSinglePlugin/components/MediaSingleReactView.js
@@ -1,22 +1,14 @@
-import React, {useEffect, useState} from 'react';
-import getStyle from "../getStyle";
-import isMouseDownEventFromResize from "../isMouseDownEventFromResize";
-import ResizableImage from "./ResizableImage";
+import React, { useState } from 'react';
+import getStyle from '../getStyle';
+import isMouseDownEventFromResize from '../isMouseDownEventFromResize';
+import ResizableImage from './ResizableImage';
 import LayoutPopover from './LayoutPopover';
-import Popover from '../../../components/Popover';
-import Image from './Image';
 
 const mediaClassName = "media";
 
 function MediaSingleReactView({id, focus, src, title, onLayoutChange, width: _width, height: _height, layout, onImageClick, onResizeEnd}) {
     const [width, setWidth] = useState(_width);
     const [height, setHeight] = useState(_height);
-    const [anchorEl, setAnchorEl] = useState(null);
-
-    useEffect(e => {
-      const img = document.querySelector(`[id="${id}"] .${mediaClassName}`);
-      setAnchorEl(img);
-    });
 
     return <div id={id} style={getStyle(layout)}>
         <div onMouseDown={e => {
@@ -25,27 +17,21 @@ function MediaSingleReactView({id, focus, src, title, onLayoutChange, width: _wi
             }
         }}>
           <div className={mediaClassName}>
-              {
-                  focus ? <ResizableImage
-                      enableToResize={focus}
-                      src={src}
-                      title={title}
-                      width={width}
-                      height={height}
-                      setWidth={setWidth}
-                      setHeight={setHeight}
-                      onResizeEnd={onResizeEnd}
-                      onImageClick={onImageClick}
-                  /> :
-                  <Image title={title} src={src} width={width} height={height} onClick={onImageClick} />
-              }
+            <ResizableImage
+                  enableToResize={focus}
+                  src={src}
+                  title={title}
+                  width={width}
+                  height={height}
+                  setWidth={setWidth}
+                  setHeight={setHeight}
+                  onResizeEnd={onResizeEnd}
+                  onImageClick={onImageClick}
+              />
+              { focus && <LayoutPopover layout={layout} onLayoutChange={layout=>{
+                onLayoutChange(layout);
+              }}/> }
           </div>
-
-          { focus && <Popover id={`MediaSingle_Popover_${id}`} anchorEl={anchorEl}>
-            <LayoutPopover layout={layout} onLayoutChange={layout=>{
-              onLayoutChange(layout);
-            }}/>
-          </Popover> }
         </div>
     </div>
 }
