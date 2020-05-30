113ff16e849273d614083352f066eb188a6bf678
scott <mymislife@gmail.com>
2020-05-30 22:08:41 +0100

    refined

--- src/packages/Aditor/plugins/GapCursorPlugin/utils/toDOM.js
+++ src/packages/Aditor/plugins/GapCursorPlugin/utils/toDOM.js
@@ -5,58 +5,6 @@ function isLeftCursor(side){
   return side === Side.LEFT;
 }
 
-function measureValue(style, measureValues){
-  const [base, ...contentBoxValues] = measureValues;
-  const measures = [style.getPropertyValue(base)];
-
-  const boxSizing = style.getPropertyValue('box-sizing');
-  if (boxSizing === 'content-box') {
-    contentBoxValues.forEach(value => {
-      measures.push(style.getPropertyValue(value));
-    });
-  }
-
-  let result = 0;
-  for (let i = 0; i < measures.length; i++) {
-    result += parseFloat(measures[i]);
-  }
-  return result;
-}
-
-function measureWidth(style){
-  return measureValue(style, [
-    'width',
-    'padding-left',
-    'padding-right',
-    'border-left-width',
-    'border-right-width',
-  ]);
-}
-
-function measureHeight(style){
-  return measureValue(style, [
-    'height',
-    'padding-top',
-    'padding-bottom',
-    'border-top-width',
-    'border-bottom-width',
-  ]);
-}
-
-function mutateElementStyle(gapCursorElem, dom, style, side){
-  if (isLeftCursor(side)) {
-    const {left} = dom.getBoundingClientRect();
-    gapCursorElem.style.left = left+"px";
-  } else {
-    // const marginRight = parseFloat(style.getPropertyValue('margin-right'));
-    // if (marginRight > 0) {
-    //   element.style.marginLeft = `-${Math.abs(marginRight)}px`;
-    // } else {
-    //   element.style.paddingRight = `${Math.abs(marginRight)}px`;
-    // }
-  }
-}
-
 function toDOM(view, getPos){
   const selection = view.state.selection;
   const { side } = selection;
